d7ef148c93c6a8e75624710484df8f9a9286eda0
thanhlv <thanhlv@d-soft.com.vn>
2020-11-24 09:16:06 +0700

    refactor

--- split.py
+++ split.py
@@ -1,15 +1,20 @@
 import os
+# Preprocessed vlsp data path
+path = '/home/thanhlv/workspace/projects/vlsp/home_work/vi_spacy/vlsp_data'
+# Split data for training path
+train_data_path = '/home/thanhlv/workspace/projects/vlsp/home_work/vi_spacy/reprocessed_data'
 
-path = '/home/thanhlv/workspace/projects/vlsp/Data'
 en_path = path + '/en.txt'
 vi_path = path + '/vi.txt'
 
-with open(en_path, 'r') as file:
+with open(vi_path, 'r') as file:
     data_line = file.readlines()
     lines_number = len(data_line)
-    with open(path + '/train.en', 'w') as train_f:
-        with open(path + '/test.en', 'w') as test_f:
-            with open(path + '/val.en', 'w' ) as val_f:
+
+    with open(train_data_path + '/train.vi', 'w') as train_f:
+        with open(train_data_path + '/test.vi', 'w') as test_f:
+            with open(train_data_path + '/val.vi', 'w' ) as val_f:
+
                 for i, value in enumerate(data_line):
                     if i < int(lines_number*0.7):
                             train_f.write(value)
